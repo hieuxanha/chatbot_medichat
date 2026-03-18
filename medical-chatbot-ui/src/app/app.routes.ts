a3c376f7efb753837e79a5415c6a9a3d24fa661b
hieu2004@ <nguyenconghieu7924@gmail.com>
2026-03-18 17:44:03 +0700

    update app.routes

--- medical-chatbot-ui/src/app/app.routes.ts
+++ medical-chatbot-ui/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
   // Khi vào đường dẫn /login thì hiện LoginComponent
   { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
   
   // Khi vào trang chủ (rỗng) thì tự động chuyển hướng sang /login
   { path: '', redirectTo: '/login', pathMatch: 'full' },
